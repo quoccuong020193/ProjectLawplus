c15d242d0b04806fc15f04399bc07a77bdb5b811
unknown <cuongdq02011993@gmail.com>
2020-10-23 21:51:00 +0700

    project

---
+++ js/main.js
@@ -0,0 +1,37 @@
+
+
+// var wrapperTitle = Array.from(document.querySelectorAll('.human'));
+
+
+// wrapperTitle.forEach(function(human) {
+//     // var humanImage = human.querySelector('.human-image');
+//     human.querySelector('.human-image').style.height = human.offsetWidth + 'px';
+//     // console.log(humanImage);
+//     // console.log("human", human, human.offsetWidth);
+// })
+
+
+
+// div class="wrapper-title d-flex">
+//             <div class="">
+//               <div class="text-title ">
+//                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur reiciendis dicta rerum qui in, ullam odio nesciunt quas quod aliquam sit adipisci, modi quisquam expedita nemo, blanditiis consectetur iure est provident distinctio fugiat. Officia at, quia temporibus veniam ducimus cum praesentium et fuga dolor sapiente laboriosam doloremque suscipit modi repellat.</p> 
+//                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus adipisci mollitia voluptate doloremque minima quisquam sit corrupti repudiandae commodi impedit?</p>
+//                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur reiciendis dicta rerum qui in, ullam odio nesciunt quas quod aliquam sit adipisci, modi quisquam expedita nemo, blanditiis consectetur iure est provident distinctio fugiat. Officia at, quia temporibus veniam ducimus cum praesentium et fuga dolor sapiente laboriosam doloremque suscipit modi 
+//                repellat.</p> 
+//                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur reiciendis dicta rerum qui in, ullam odio nesciunt quas quod aliquam sit adipisci, modi quisquam expedita nemo, blanditiis consectetur iure est provident distinctio fugiat. Officia at, quia temporibus veniam ducimus cum praesentium et fuga dolor sapiente laboriosam doloremque suscipit modi repellat.</p> 
+//                <div class="btn-title d-flex">
+//                  <a class="button btn-text1" href="#">read more</a>
+//                  <a class="button btn-text1" href="">read more</a>
+//                </div>
+//             </div>
+//             <div class="">
+//                 <div class="human">
+//                   <div class="human-image">
+//                     <a href=""> <img src="images/human.png" alt=""></a>
+//                   </div>
+//                 </div>
+//             </div>
+//           </div>
+//           </div>
+//          </div>
